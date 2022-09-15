c3e9b2e4bf8c6a326295341e860c1c2114be3caf
chen <1354005772@qq.com>
2022-09-16 01:38:44 +0800

    kankan

---
+++ src/main/java/org/example.java
@@ -0,0 +1,12 @@
+package org;
+
+/**
+ * @author czh
+ * @version 1.0
+ * @date 2022/9/16 1:35
+ */
+public class example {
+    public static void main(String[] args) {
+        System.out.println("0909090");
+    }
+}
