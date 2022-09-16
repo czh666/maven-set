608530544a72cb4146fa87f36e61ae008aecbe4d
chen <1354005772@qq.com>
2022-09-16 20:24:32 +0800

    aoligei

--- src/main/java/org/example.java
+++ src/main/java/org/example.java
@@ -14,5 +14,6 @@ public class example {
         System.out.println("hot-fix");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("9999");
     }
 }
